58ed8d6bc05b4ca97648bb9dee5dbe88a4305b47
krka02 <krisztian.kapitany@betssongroup.com>
2017-01-06 16:06:30 +0100

    Adding card info

--- public/app.js
+++ public/app.js
@@ -20,4 +20,37 @@ angular.module('sortApp', [])
     return JSON.stringify(card, undefined, 4);
   }
 
+  $scope.saveToPc = function (filename) {
+
+    var data = $scope.cards
+
+    if (!filename) {
+      filename = 'download.json';
+    }
+
+    if (typeof data === 'object') {
+      data = JSON.stringify(data, undefined, 2);
+    }
+
+    var blob = new Blob([data], {type: 'text/json'});
+
+    // FOR IE:
+
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(blob, filename);
+    }
+    else{
+        var e = document.createEvent('MouseEvents'),
+            a = document.createElement('a');
+
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+        e.initEvent('click', true, false, window,
+            0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+    }
+  };
+  
+  
 });
